c1a4b271c04736242fd02ed92a5c701004860d17
traviswye <traviswye@gmail.com>
2025-04-02 07:55:26 -0400

    minor script updates

--- Scripts/DailyFlowCF/yesterdaysresults_cf.py
+++ Scripts/DailyFlowCF/yesterdaysresults_cf.py
@@ -727,7 +727,7 @@ if __name__ == "__main__":
     else:
         # Default values
         start_date = '2025-03-31'
-        end_date = '2025-03-31'
+        end_date = '2025-04-01'
 
     # Call the main function with either command-line arguments or defaults
     main(start_date=start_date, end_date=end_date)
